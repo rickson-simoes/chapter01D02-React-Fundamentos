e04a24523e14899a78761d388f668870625278d0
Rickson <rickson.simoes@hotmail.com>
2021-09-24 09:47:31 -0300

    :hammer: Desafio 100%

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,45 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../components/MovieCard';
+import { api } from '../services/api';
+
+interface IContentProps {
+  title: string;
+  genreId: number;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: IContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.genreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [props.genreId]);
+
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {props.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,20 @@
-export function SideBar() {
-  // Complete aqui
+import { HtmlHTMLAttributes } from "react";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({children}:HtmlHTMLAttributes<GenreResponseProps>) {
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {children}
+    </div>
+
+  </nav>
+  )
 }
